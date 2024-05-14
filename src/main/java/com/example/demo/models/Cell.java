97eff28afbca8e0319965f330cb1245c710df9d5
bozhovskiy <roman.bozhovskyi.pm.2021@lpnu.ua>
2024-05-14 14:03:33 +0300

    Fix

---
+++ src/main/java/com/example/demo/models/Cell.java
@@ -0,0 +1,14 @@
+package com.example.demo.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Cell {
+    private int x;
+    private int y;
+    private int value;
+}
